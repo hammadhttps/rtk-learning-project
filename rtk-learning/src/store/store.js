3538af7a854701ec12bed76d476e2c58ad4d8425
Hammad-ul-Hassan <103960660+HammadulHassan5812@users.noreply.github.com>
2025-08-20 16:44:22 +0500

    add reducers for posts, auth, and stories to the Redux store

--- rtk-learning/src/store/store.js
+++ rtk-learning/src/store/store.js
@@ -1,4 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postReducer from "../app/features/posts/postSlice.js";
+import authReducer from "../app/features/auth/authSlice.js";
+import storiesReducer from "../app/features/stories/storiesSlice.js";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postReducer,
+    auth: authReducer,
+    stories: storiesReducer,
+  },
 });
